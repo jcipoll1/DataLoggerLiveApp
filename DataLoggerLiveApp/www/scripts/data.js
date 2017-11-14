3776aa8291370e605073a8047459cf7704f0da37
Joseph Cipollina <jcipoll1@binghamton.edu>
2017-11-13 23:01:42 -0500

    Added www\scripts\data.js instead of weather.js with weather app code

---
+++ DataLoggerLiveApp/www/scripts/data.js
@@ -0,0 +1,47 @@
+﻿var OpenWeatherAppKey = "Your_Key_Here";
+
+function getWeatherWithZipCode() {
+
+    var zipcode = $('#zip-code-input').val();
+
+    var queryString =
+        'http://api.openweathermap.org/data/2.5/weather?zip='
+        + zipcode + ',us&appid=' + OpenWeatherAppKey + '&units=imperial';
+
+    $.getJSON(queryString, function (results) {
+
+        showWeatherData(results);
+
+    }).fail(function (jqXHR) {
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. " + jqXHR.statusText);
+    });
+
+    return false;
+}
+
+function showWeatherData(results) {
+
+    if (results.weather.length) {
+
+        $('#error-msg').hide();
+        $('#weather-data').show();
+
+        $('#title').text(results.name);
+        $('#temperature').text(results.main.temp);
+        $('#wind').text(results.wind.speed);
+        $('#humidity').text(results.main.humidity);
+        $('#visibility').text(results.weather[0].main);
+
+        var sunriseDate = new Date(results.sys.sunrise * 1000);
+        $('#sunrise').text(sunriseDate.toLocaleTimeString());
+
+        var sunsetDate = new Date(results.sys.sunset * 1000);
+        $('#sunset').text(sunsetDate.toLocaleTimeString());
+
+    } else {
+        $('#weather-data').hide();
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. ");
+    }
+}
