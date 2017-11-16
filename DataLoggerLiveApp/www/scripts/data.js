4dcffd723713967a66974bccf7dcc9702dd4eeb6
Joseph Cipollina <jcipoll1@binghamton.edu>
2017-11-15 23:20:49 -0500

    This program simulates with the raspberry pi commanded with $ sudo http-server -p 80

--- DataLoggerLiveApp/www/scripts/data.js
+++ DataLoggerLiveApp/www/scripts/data.js
@@ -1,30 +1,37 @@
-﻿// var OpenWeatherAppKey = "Your_Key_Here";
-
-function getDataFromDataLogger() {
+﻿function getDataFromDataLogger() {
 
     var datalogger = $('#data-logger-input').val();
 
     var datalistname = 'time';
 
     var queryString =
-        'https://'
-        + datalogger + ':443/' + datalistname + '.json';
+        'http://'
+        + datalogger
+//        + ':443'
+        + '/' + datalistname + '.json';
+    /*
+    var OpenWeatherAppKey = "cd2c92a41ccfcdee1b918f78167d67bf";
+    var zipcode = '13901';
+
+   queryString =
+        'http://api.openweathermap.org/data/2.5/weather?zip='
+        + zipcode + ',us&appid=' + OpenWeatherAppKey + '&units=imperial';*/
 
     $.getJSON(queryString, function (results) {
 
         showData(results);
 
-    }).fail(function (jqXHR) {
-        $('#error-msg').show();
-        $('#error-msg').text("Error retrieving data. " + jqXHR.statusText);
-    });
+        }).fail(function (jqXHR) {
+            $('#error-msg').show();
+            $('#error-msg').text("Error retrieving data. " + jqXHR.statusText);
+        });
 
-    return false;
+        return false;
 }
 
 function showData(results) {
 
-    if (results.length) {
+    if (results.hasOwnProperty.length) {
 
         $('#error-msg').hide();
         $('#data-logger-data').show();
